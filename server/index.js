3ebaa620f9d0df4b28826a6216b383a76d27b55f
amandekate <56401874+amandekate@users.noreply.github.com>
2023-04-08 20:18:40 +0530

    Added project and updated link

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
-// const express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 
@@ -16,9 +16,8 @@ const bodyParser = require('body-parser');
 // Express 4.0
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.json());
 
-// const app = express();
+const app = express();
 app.use(cors());
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000 }));
