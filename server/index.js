21d6de6e3112134e1f42215e07aa4f894e4c1391
amandekate <56401874+amandekate@users.noreply.github.com>
2023-04-08 20:27:57 +0530

    limit set for payload function

--- server/index.js
+++ server/index.js
@@ -8,19 +8,11 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-
-
-// Express 4.0
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 const app = express();
 app.use(cors());
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000 }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({limit: "50mb", extended: false, parameterLimit:100000 }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
