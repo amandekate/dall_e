f9eb56103d62420063f3c68195cb1d1b90f4ca6f
amandekate <56401874+amandekate@users.noreply.github.com>
2023-04-08 20:08:50 +0530

    func payload

--- server/index.js
+++ server/index.js
@@ -8,11 +8,19 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express();
+
+
+// Express 4.0
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+// const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000 }));
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000 }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
