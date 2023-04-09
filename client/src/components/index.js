1ce2e171c9f9924150d69afa6e605654b45b1496
amandekate <56401874+amandekate@users.noreply.github.com>
2023-04-09 21:21:10 +0530

    download image button

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,8 @@
 import Card from "./Card";
 import FormField from "./FormField";
 import Loader from "./Loader";
-import CreatePost from "../pages/CreatePost";
 export{
     Card,
     FormField,
     Loader,
-    CreatePost
 }
