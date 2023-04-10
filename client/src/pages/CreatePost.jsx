132a757cf789260d339b3cd0fa6870d328ebd282
amandekate <56401874+amandekate@users.noreply.github.com>
2023-04-10 15:46:18 +0530

    final test commit- 4

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -153,12 +153,12 @@ const CreatePost = () => {
           </button>
         </div>
 
-        <div className="mt-5 flex gap-5">
-          <DownloadButton
+        {/* <div className="mt-5 flex gap-5">
+          {/* <DownloadButton
             onClick={(e) => downloadsImage(e, form.photo, form.prompt)}
             className="text-white bg-blue-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           />
-        </div>
+        </div> */} 
 
         <div className="mt-10">
           <p className="mt-2 text-[#666e75] text-[14px]">
